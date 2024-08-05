ee7f93e4a39e096a3c016c97a7b9478cf4f0f6d6
Daria Borisiak <daria.borisiak@gmail.com>
2024-08-05 22:23:32 +0200

    Add NewtonSqrtApproximation

--- src/main/scala/example/main.scala
+++ src/main/scala/example/main.scala
@@ -1,4 +1,6 @@
 import example.Lists
+import example.NewtonSqrtApproximation
 
 @main def run(): Unit =
-  println(Lists.max(List(1,3,2,6,7,87,9)))
+  // println(Lists.max(List(1,3,2,6,7,87,9)))
+  println(NewtonSqrtApproximation.sqrt(2));
