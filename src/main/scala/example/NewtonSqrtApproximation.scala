a86a2cddf4a7a9a266c362a8f4b1400e220d5b9b
Daria Borisiak <daria.borisiak@gmail.com>
2024-08-05 22:41:26 +0200

    Improve Newton's method

--- src/main/scala/example/NewtonSqrtApproximation.scala
+++ src/main/scala/example/NewtonSqrtApproximation.scala
@@ -1,15 +1,17 @@
 package example
 
-object NewtonSqrtApproximation:
-  def abs(x: Double): Double = if (x < 0) -x else x
+import scala.annotation.tailrec
 
-  def sqrtIter(guess: Double, x: Double): Double =
+object NewtonSqrtApproximation:
+  private def abs(x: Double): Double = if (x < 0) -x else x
+
+  @tailrec private def sqrtIter(guess: Double, x: Double): Double =
     if (isGoodEnough(guess, x)) guess
     else sqrtIter(improve(guess, x), x)
 
-  def isGoodEnough(guess: Double, x: Double) = abs(guess * guess - x) < 0.0001
+  private def isGoodEnough(guess: Double, x: Double) = (abs(guess * guess - x) / x) < 0.0001
 
-  def improve(guess: Double, x: Double) = (guess + x / guess) / 2
+  private def improve(guess: Double, x: Double) = (guess + x / guess) / 2
 
-  def sqrt(x: Double) = sqrtIter(1.0, x)
+  def sqrt(x: Double): Double = sqrtIter(1.0, x)
 
--- src/main/scala/example/main.scala
+++ src/main/scala/example/main.scala
@@ -1,6 +1,12 @@
 import example.Lists
 import example.NewtonSqrtApproximation
+import scala.math._
 
 @main def run(): Unit =
+  // val x = 0.001
+  // val x = 0.1e-20
+  val x = 1.0e50
+
   // println(Lists.max(List(1,3,2,6,7,87,9)))
-  println(NewtonSqrtApproximation.sqrt(2));
+  println(NewtonSqrtApproximation.sqrt(x));
+  println(sqrt(x))
